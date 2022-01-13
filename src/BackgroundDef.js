7103d362846d35ddacdbfd3dc47e354264d12c6b
Andrew Miller <info@akmiller.co.uk>
2022-01-13 19:20:38 +0000

    prettier

--- src/BackgroundDef.js
+++ src/BackgroundDef.js
@@ -1,48 +1,60 @@
-import React from 'react'
-import {number, string, bool} from 'prop-types'
+import React from "react";
+import { number, string, bool } from "prop-types";
 
 function getSize(props) {
-  let width = props.diagonal
-  let height = props.diagonal
+  let width = props.diagonal;
+  let height = props.diagonal;
 
   if (props.flatTop) {
-    width *= 1.05
-    height *= 1.05
+    width *= 1.05;
+    height *= 1.05;
   }
 
   if (props.backgroundScale) {
-    width *= props.backgroundScale
-    height = width
+    width *= props.backgroundScale;
+    height = width;
   } else if (props.backgroundWidth) {
-    width = props.backgroundWidth
-    height = props.backgroundHeight
+    width = props.backgroundWidth;
+    height = props.backgroundHeight;
   } else if (props.backgroundSize) {
-    width = props.backgroundSize
-    height = props.backgroundSize
+    width = props.backgroundSize;
+    height = props.backgroundSize;
   }
 
-  return {width, height}
+  return { width, height };
 }
 
 function BackgroundDef(props) {
-  const diagonal = props.diagonal
-  const {width, height} = getSize(props)
+  const diagonal = props.diagonal;
+  const { width, height } = getSize(props);
 
-  const hasSize = width !== diagonal
-  const imgWidth = hasSize ? width : '100%'
-  const imgHeight = hasSize ? height : '100%'
-  const yOffset = props.yOffset || 0
-  const xOffset = typeof props.xOffset === 'undefined' && props.flatTop
-    ? 0 - (imgWidth * 0.065)
-    : 0
+  const hasSize = width !== diagonal;
+  const imgWidth = hasSize ? width : "100%";
+  const imgHeight = hasSize ? height : "100%";
+  const yOffset = props.yOffset || 0;
+  const xOffset =
+    typeof props.xOffset === "undefined" && props.flatTop
+      ? 0 - imgWidth * 0.065
+      : 0;
 
   return (
     <defs>
-      <pattern id={props.id} width={width} height={height} patternUnits="userSpaceOnUse">
-        <image x={xOffset} y={yOffset} width={imgWidth} height={imgHeight} xlinkHref={props.backgroundImage} />
+      <pattern
+        id={props.id}
+        width={width}
+        height={height}
+        patternUnits="userSpaceOnUse"
+      >
+        <image
+          x={xOffset}
+          y={yOffset}
+          width={imgWidth}
+          height={imgHeight}
+          xlinkHref={props.backgroundImage}
+        />
       </pattern>
     </defs>
-  )
+  );
 }
 
 /* eslint-disable react/no-typos */
@@ -56,7 +68,7 @@ BackgroundDef.propTypes = {
   backgroundSize: number,
   xOffset: number,
   yOffset: number,
-  diagonal: number
-}
+  diagonal: number,
+};
 
-export default BackgroundDef
+export default BackgroundDef;
